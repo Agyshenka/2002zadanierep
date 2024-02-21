9041b04b9af8dbedba66b437c769da1662ad3f75
Agyshenka <yaneagysha@mail.ru>
2024-02-21 18:48:28 +0300

    Initial

---
+++ WpfApp21231/mathLogics/fact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp21231.mathLogics
+{
+    internal class fact
+    {
+    }
+}
