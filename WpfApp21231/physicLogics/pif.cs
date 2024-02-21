1934ef65002ae69dd1b48336a6304b541c459123
Agyshenka <yaneagysha@mail.ru>
2024-02-21 18:52:40 +0300

    Added hypotinuse logic

--- WpfApp21231/physicLogics/pif.cs
+++ WpfApp21231/physicLogics/pif.cs
@@ -8,5 +8,12 @@ namespace WpfApp21231.physicLogics
 {
     internal class pif
     {
+        static double CalculateHypotenuse(int a, int b, int angle)
+        {
+            double c = Math.Pow(a, 2) + Math.Pow(b, 2) - (2 * a * b) * Math.Cos(angle);
+            return Math.Sqrt(c);
+        }
+
+
     }
 }
