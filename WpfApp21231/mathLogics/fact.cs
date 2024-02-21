d8c914b702d69595ecc12d5c12981953d71b2f78
Agyshenka <yaneagysha@mail.ru>
2024-02-21 18:51:25 +0300

    Added factorial logic

--- WpfApp21231/mathLogics/fact.cs
+++ WpfApp21231/mathLogics/fact.cs
@@ -8,5 +8,15 @@ namespace WpfApp21231.mathLogics
 {
     internal class fact
     {
+        public static int Calculate(int n)
+        {
+            int res = 1;
+            for (int i = 0; i < n; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
+
     }
 }
